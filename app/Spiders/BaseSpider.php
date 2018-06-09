c52d14475bc307d50d044dcb69f31b65fd73a032
Silas Vasconcelos <silasvasconcelos@hotmail.com.br>
2018-06-09 18:12:53 -0300

    Function to helper Spiders

--- app/Spiders/BaseSpider.php
+++ app/Spiders/BaseSpider.php
@@ -2,11 +2,22 @@
 
 namespace App\Spiders;
 
+use Symfony\Component\DomCrawler\Crawler;
+use GuzzleHttp\Client;
+
+
 /**
  * Spider to guard shared methods
  */
 class BaseSpider
 {
+	private $httClient;
+
+	function __construct()
+	{
+		$this->httClient = new Client;	
+	}
+
 	public function getEnvURL(string $env_name) : string
 	{
 		$url =  env($env_name);
@@ -18,4 +29,34 @@ class BaseSpider
 		}
 		return $url;
 	}
+
+	function get(string $url, array $options=[])
+	{
+		return $this->request($url, 'GET', $options);
+	}
+
+	function post(string $url, array $options=[])
+	{
+		return $this->request($url, 'POST', $options);
+	}
+
+	function put(string $url, array $options=[])
+	{
+		return $this->request($url, 'PUT', $options);
+	}
+
+	function request(string $url, string $method, array $options=[])
+	{
+		$res = $this->httClient->request($method, $url, $options);
+		if ($res->getStatuscode() !== 200) {
+			return;
+		}
+		return $res->getBody()->getContents();		
+	}
+
+	function parse($toParse) : Crawler
+	{
+		return new Crawler($toParse);
+	}
+
 }
